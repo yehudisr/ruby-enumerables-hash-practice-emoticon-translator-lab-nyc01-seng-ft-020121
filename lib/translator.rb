7a96361633c5bd37f5a88e5c701fc9f2f04598a9
yehudis raitman <yehudisr@gmail.com>
2021-01-24 20:52:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,6 +16,7 @@ end
 def get_japanese_emoticon(file, english)
   # code goes here
   get_japanese = YAML.load_file(file)
+  japanese_emoji = 
   get_japanese.each do |key, value|
     get_japanese[key] = {:english => value[0], :japanese => value[1]}
   binding.pry 
