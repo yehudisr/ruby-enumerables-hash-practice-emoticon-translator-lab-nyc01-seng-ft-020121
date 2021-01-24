e08015f0dc7e3ff7dcd3cac5cd9918904a3b5d48
yehudis raitman <yehudisr@gmail.com>
2021-01-24 21:16:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,19 +4,25 @@ require "yaml"
 emoticons = YAML.load_file('lib/emoticons.yml')
 file = 'lib/emoticons.yml'
 
-def load_library(file)
+
   # turn array into hash with two keys
-  emoticons = {"get_meaning"=>{}, "get_emoticon" => {}}
-  
-  YAML.load_file(file).each |meaning, describe|
-  #eng, jan = describe
-  emoticons["get_meaning"][jan] = meaning
-  emoticons["get_emoticon"][eng] = jan
-  
+ def load_library(path)
+  emoticons = YAML.load_file(path)
+  emoticon_hash = Hash.new
+
+  emoticon_hash["get_emoticon"] = Hash.new
+  emoticon_hash["get_meaning"] = Hash.new
+
+  emoticons.each do |english_word, emoticon_set|
+    emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
+    emoticon_hash["get_meaning"][emoticon_set.last] = english_word
+  end
+  emoticon_hash
   binding.pry
 end
-emoticons
-end
+  
+  
+
   
   emoticons.each do |key, value|
     dictionary[key] = {:english => "", :japanese => ""}
