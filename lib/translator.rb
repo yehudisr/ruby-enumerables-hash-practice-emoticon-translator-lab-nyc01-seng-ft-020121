061f64ecf270d8d3f8288b75f62dfb823395a308
yehudis raitman <yehudisr@gmail.com>
2021-01-24 20:54:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,6 +19,7 @@ def get_japanese_emoticon(file, english)
   japanese_emoji = 
   get_japanese.each do |key, value|
     get_japanese[key] = {:english => value[0], :japanese => value[1]}
+    
   binding.pry 
      
   end  
