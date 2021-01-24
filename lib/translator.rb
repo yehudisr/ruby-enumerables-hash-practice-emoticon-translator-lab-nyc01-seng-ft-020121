ff8ccd5795f10747fbd5a33695c02b161a58d366
yehudis raitman <yehudisr@gmail.com>
2021-01-24 22:30:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_japanese_emoticon(file, english)
   load_library(file).each do |meaning, emojis|
     emojis.each do |lang, pic| 
       if 
-      end
+      english == 
       end
        binding.pry
   end  
@@ -34,7 +34,7 @@ def get_english_meaning(file, japanese)
   load_library(file).each do |meaning, emojis|
     
   emojis.each do |lang, pic|  
-    if  japanese = pic[:japanese] 
+    if  japanese == pic[:japanese] 
       meaning
     elseif
       sorry
